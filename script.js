bb386b10fe88de06a5fb8647dc181de751593584
Joshrobertson023 <therealjoshrobertson@gmail.com>
2023-10-26 12:12:27 -0500

    Added loop to render boxes and rows

--- script.js
+++ script.js
@@ -23,17 +23,15 @@ let numSquares = 16; // Change this later to be the input
 /*                             Main program                           */
 /**********************************************************************/
 
-// If the squares are 16, create a 16 by 16 grid
-// Write a loop to create 16 squares right, then 16 rows down
-// These squares must have specific CSS properties to fill the container and all grow and shrink the same size
-
-// 1: Create a row container for those boxes
-// Append the row container in the big container
-// 2: Create a box
-// 3: Append box to row container
-// 4: Loop back to step 2 until have 16 boxes
-// 5: Loop back to step 1 until have 16 rows
+/* Loop processing sketch boxes and rows */
 for (let i = 0; i < numSquares; i++) {
    const row = document.createElement('div');
+   row.classList.add('row');
    container.appendChild(row);
+
+   for (let j = 0; j < numSquares; j++) {
+      const box = document.createElement('div');
+      row.appendChild(box);
+      box.classList.add('box');
+   }
 }
