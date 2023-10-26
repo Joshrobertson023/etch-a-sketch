d57f7e5b1915d1bce30c2e344a50645ee426ac78
Joshrobertson023 <therealjoshrobertson@gmail.com>
2023-10-26 13:49:09 -0500

    Program finished and running as intended.
    Some things could be improved later.

--- script.js
+++ script.js
@@ -12,26 +12,65 @@
 /**********************************************************************/
 /*                            DOM constants                           */
 /**********************************************************************/
-const container = document.getElementById('container');
+const container = document.getElementById('container'),
+      btnRender = document.getElementById('btnrender');
 
 /**********************************************************************/
 /*                          Global variables                          */
 /**********************************************************************/
-let numSquares = 16; // Change this later to be the input
+let numSquares = 16;
 
 /**********************************************************************/
 /*                             Main program                           */
 /**********************************************************************/
+renderGrid(numSquares); // Open document with a 16x16 grid already rendered
 
-/* Loop processing sketch boxes and rows */
-for (let i = 0; i < numSquares; i++) {
-   const row = document.createElement('div');
-   row.classList.add('row');
-   container.appendChild(row);
+btnRender.addEventListener('click', () => {
+   numSquares = getNumber();
+   clearGrid();
+   renderGrid(numSquares);
+});
 
-   for (let j = 0; j < numSquares; j++) {
-      const box = document.createElement('div');
-      row.appendChild(box);
-      box.classList.add('box');
-   }
+box.addEventListener('click', () => {
+   box.style.backgroundColor = "black";
+})
+
+/**********************************************************************/
+/*                         Remove all rows                            */
+/**********************************************************************/
+function clearGrid() {
+   const rows = container.querySelectorAll('.row');
+   rows.forEach((row) => {
+      container.removeChild(row);
+   });
+}
+
+/**********************************************************************/
+/*                       Render boxes and rows                        */
+/**********************************************************************/
+function renderGrid(numIterations) {
+   for (let i = 0; i < numIterations; i++) {
+      let row = document.createElement('div');
+      row.classList.add('row');
+      container.appendChild(row);
+
+      for (let j = 0; j < numIterations; j++) {
+         let box = document.createElement('div');
+         row.appendChild(box);
+         box.classList.add('box');
+         box.addEventListener('mouseover', () => {
+            box.style.backgroundColor = 'black';
+         })
+      }
+   }  
+}
+
+/**********************************************************************/
+/*                     Get the number of squares                      */
+/**********************************************************************/
+function getNumber() {
+   do {
+      numSquares = parseInt(document.getElementById('input').value);
+   } while (numSquares < 4 || numSquares > 100);
+   return numSquares;
 }
